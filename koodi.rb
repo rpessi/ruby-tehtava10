b60098e98201a95e2e12e2c6f05998e191da2c66
Riitta Pessi <riitta@pessi.fi>
2025-03-14 17:19:19 +0200

    Ruby harjoittelutehtävä 10

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,10 @@
-# kirjoita koodi tänne
+# Tee metodi avainten_summa, joka saa parametriksi hashin, jonka kaikki avaimet ovat kokonaislukuja,
+# ja palauttaa sen avainten summan. Etsi sopivia metodeja Hashin apikuvauksesta
+
+def avainten_summa(hash)
+  summa = 0
+  hash.keys.each do |avain|
+    summa = summa + avain
+  end
+return summa
+end
